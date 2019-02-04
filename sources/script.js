4563537ef7ed020926b37d71c74e8c032f747474
Syk Houdeib <syk.houdeib@gmail.com>
2019-02-04 20:44:02 +0100

    [NEW] Add class to clicked cell

--- sources/script.js
+++ sources/script.js
@@ -2,6 +2,11 @@ const cells = document.querySelectorAll('[data-select="play-area"]')
 
 cells.forEach( cell => {
     cell.addEventListener( 'click', event => {
-        console.log('event: ', event)
+        markCell(event.currentTarget)
     })
 })
+
+function markCell(target) {
+    target.classList.add('marked')
+    console.log(target)
+}
